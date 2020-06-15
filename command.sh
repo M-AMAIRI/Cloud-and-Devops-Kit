14a57b98eb378b30ecf43bac75edb536bc1fe12f
moatez amairi <35851660+M-AMAIRI@users.noreply.github.com>
2020-06-15 00:03:41 -0400

    Update command.sh

--- command.sh
+++ command.sh
@@ -35,7 +35,9 @@ kubectl get nodes -o jsonpath='{.items[*].status.nodeInfo.osImage}' > test.txt
 
 
 
-
+ETCDCTL_API=3 etcdctl --endpoints=https://[127.0.0.1]:2379 --cacert=/etc/kubernetes/pki/etcd/ca.crt \
+     --cert=/etc/kubernetes/pki/etcd/server.crt --key=/etc/kubernetes/pki/etcd/server.key \
+     snapshot save /tmp/etcd-backup.db
 
 
 
